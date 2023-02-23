cd5b2fd138c3f00764f06e46b0ec7d21d50c1ad5
nigelnix <nigel.nix@outlook.com>
2023-02-23 22:02:28 +0100

    added fetch and map to display vans array

--- .history/index_20230223194637.jsx
+++ .history/index_20230223194637.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 
-import "./server"
-
 function App() {
   return (
     <BrowserRouter>
